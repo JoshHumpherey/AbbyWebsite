733386f64b4aa95f9adc3ad455a8112a6c1c0094
JoshHumpherey <jhumphrey321@gmail.com>
2018-02-27 16:22:10 -0500

    Added in /Josh page and also figured out the google maps api integration

--- WebApp.py
+++ WebApp.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return render_template('mainpage.html')
+@app.route("/Abby")
+def AbbyWebpage():
+    return render_template('abbyMainpage.html')
 
+@app.route("/Josh")
+def JoshWebpage():
+    return render_template('joshMainpage.html')
 
 if __name__ == "__main__":
     app.run()
